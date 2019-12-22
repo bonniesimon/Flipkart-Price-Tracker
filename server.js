dbde575ac6e96e3b935633fad1d1a891812e7147
Bonnie Simon <bonniesimon@protonmail.com>
2019-12-22 11:20:52 +0530

    Refactored code into reusable functions a.k.a DRY principle

--- server.js
+++ server.js
@@ -6,21 +6,33 @@ const app = express();
 // let url = 'https://www.flipkart.com/cosmic-byte-cb-gk-02-corona-wired-7-color-rgb-backlit-effects-anti-ghosting-usb-gaming-keyboard/p/itmeyefkgwpxsmgk';
 let url = 'https://www.flipkart.com/lenovo-legion-m200-wired-optical-gaming-mouse/p/itmffegkcch6a5pp';
 
+const logStatus = (name, price, rating, availability) => {
+    console.log('Name : ',name.text());
+    console.log('Price : ',price.text());
+    console.log('Rating : ',rating.text().slice(0,4));
+    if(availability.text() == ''){
+        console.log('Availability : Available!!🤩');
+    }else{
+        console.log('Availability : ',availability.text(),'😌');
+    }
+}
 
-request(url, (error,res, html)=>{
-   const $ = cheerio.load(html);
-   let availability = $('._9-sL7L');
-   let price = $('._3qQ9m1').text();
-   console.log(price);
-   if(availability.text() == ''){
-       console.log('Item available!!');
-   }else{
-       console.log(availability.text());
-   }
+const getStatus = (urlValue) => {
+    request(urlValue, (error,res, html)=>{
+        const $ = cheerio.load(html);
+        let productName = $('._35KyD6');
+        let rating = $('.hGSR34');
+        let availability = $('._9-sL7L');
+        let price = $('._3qQ9m1');
+        logStatus(productName, price, rating, availability);
+     
+    })
+}
 
-//    console.log(typeof price);
-   console.log('after logging price');
-})
+
+getStatus(url);
+
+
 
 
 
