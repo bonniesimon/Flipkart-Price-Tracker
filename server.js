7724bf322ef22536ae843c9c85a68e7451dab57a
Bonnie Simon <bonniesimon@protonmail.com>
2020-01-07 18:01:41 +0530

    Created a database.json; And function to update the database

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const request = require('request');
 const cheerio = require('cheerio');
-const bodyParser = require('body-parser');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
 
 
 
@@ -20,10 +20,15 @@ app.use(bodyParser.json())
 // let url = 'https://www.flipkart.com/lenovo-legion-m200-wired-optical-gaming-mouse/p/itmffegkcch6a5pp';
 let url = 'https://www.flipkart.com/flipkart-smartbuy-mp406-mousepad/p/itm832563ec25943?pid=ACCFJZGXK8PWGHS8&lid=LSTACCFJZGXK8PWGHS8ZCQYYC&marketplace=FLIPKART&srno=b_1_35&otracker=hp_reco_Discounts%2Bfor%2BYou_2_7.dealCard.OMU_cid%3AS_F_N_4rr_km5_r39__d_50-100__NONE_ALL%3Bnid%3A4rr_km5_r39_%3Bmp%3AF%3Bct%3Ad%3B_5&otracker1=hp_reco_WHITELISTED_personalisedRecommendation%2Fdiscount_Discounts%2Bfor%2BYou_DESKTOP_HORIZONTAL_dealCard_cc_2_NA_view-all_5&fm=personalisedRecommendation%2Fdiscount&iid=81d62083-cd8e-4a77-a4f6-2ec46ae38ea0.ACCFJZGXK8PWGHS8.SEARCH&ppt=browse&ppn=browse&ssid=hqhen19dgiggamf41578390709805';
 
+
+
+
+
+
 const logStatus = (name, price, rating, availability) => {
     console.log('Name : ',name.text());
     console.log('Price : ',price.text());
-    console.log('Rating : ',rating.text().slice(0,4));
+    console.log('Rating : ',rating.text().slice(0,3));
     if(availability.text() == ''){
         console.log('Availability : Available!!🤩');
     }else{
@@ -31,28 +36,6 @@ const logStatus = (name, price, rating, availability) => {
     }
     
 }
-
-
-
-// let getStatus = (urlValue) => {
-//      request(urlValue, (error,res, html)=>{
-//         const $ = cheerio.load(html);
-//         let productName = $('._35KyD6');
-//         let rating = $('.hGSR34');
-//         let availability = $('._9-sL7L');
-//         let price = $('._3qQ9m1');
-//        let details = {
-//             name : productName.text(),
-//             rating : rating.text(),
-//             availability : availability.text(),
-//             price : price.text()
-//         };
-        
-//         // logStatus(productName, price,rating,availability);
-//         return details;
-     
-//     })
-// }
 
 
 
@@ -65,7 +48,9 @@ const getStatus = async (urlValue) => {
         let availability = $('._9-sL7L');
         let price = $('._3qQ9m1');
         // logStatus(productName, price,rating,availability);
-        let availabilityStatus
+
+        //just formatting the availabilty value
+        let availabilityStatus;
         if(availability.text() == ''){
             availabilityStatus =  'Available!!';
         }else{
@@ -73,10 +58,11 @@ const getStatus = async (urlValue) => {
         }
         let details = {
             name : productName.text(),
-            rating : rating.text().slice(0,4),
+            rating : rating.text().slice(0,3),
             availability : availabilityStatus,
             price : price.text()
         };
+        //Instead of simply returning as json, I returned it as a promise and it worked!
         return new Promise((resolve, reject) => {
             resolve(details);
         });
@@ -87,13 +73,36 @@ const getStatus = async (urlValue) => {
 }
 
 
+//Declaring data to be stored in json file
+let database = {};
+let val,json;
 
-let val = getStatus(url);
-val.then(result => console.log(result));
+//init function that runs when server active
+const init = () => {
+    val = getStatus(url);
+    val.then(result => {
+            console.log(result);
+            // database.dataTable.push(result);
+            // console.log(database);
+            // json = JSON.stringify(database);
+            fs.readFile('database.json',(err ,data) => {
+                if(err){
+                    console.log(err);
+                }else{
+                    //extracting the old data and parsing it to an object (which means converting it to an object)
+                    database = JSON.parse(data);
+                    //push the present data to the existing data
+                    database.dataTable.push(result);
+                    c
+                }
+                
+            })
+        });
+}
 
 
 
-
+init();
 
 app.listen(8080,()=>console.log('Server Running @ PORT 3000!!'));
 
